289881bd9b0ae7ae6f970b953cb1eaf7c7e00253
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2020-03-20 21:16:47 +0900

    add assume role.

--- src/main.tf
+++ src/main.tf
@@ -2,8 +2,14 @@ terraform {
   required_version = "0.12.24"
 }
 
+variable "assume_role" { 
+}
+
 provider "aws" {
-  region = "ap-northeast-1"
+  region     = "ap-northeast-1"
+  assume_role {
+    role_arn     = var.assume_role
+  }
 }
 
 module "network" {
